240abee2188e21ca2e7f072d3d6b22b24b58dca3
Daniel Reyes <DanielReHer@outlook.com>
2023-12-03 19:49:23 -0600

    Se agregó la función para establecer las tareas en el calendario

--- src/components/Modal-CrearTicket.jsx
+++ src/components/Modal-CrearTicket.jsx
@@ -77,7 +77,7 @@ const Modal = ({ setIsOpen }) => {
 
             closeModal();
         } catch (error) {
-            alert('Error al crear el proyecto')
+            alert('Error al crear el ticket')
             console.log(error)
         }
     }
